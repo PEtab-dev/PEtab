9c52cdefaa6964590211ce23289b506480cf473e
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2018-12-12 17:19:31 +0100

    Update setup.py. Check python version (Closes #10), add libsbml dependency (Closes #11), add valid email. Bump version number.

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 import os
+import sys
+
+# Python version check. We need >= 3.6 due to e.g. f-strings
+if sys.version_info < (3, 6):
+    sys.exit('PEtab requires at least Python version 3.6')
 
 
 # read a file
@@ -9,17 +14,19 @@ def read(fname):
 
 # project metadata
 setup(name='petab',
-      version='0.0.0',
-      description="Parameter estimation tabular data",
+      version='0.0.0a2',
+      description='Parameter estimation tabular data',
       long_description=read('README.md'),
-      author="The PEtab developers",
-      author_email="",
-      url="https://github.com/icb-dcm/petab",
-      packages=find_packages(exclude=["doc*", "test*"]),
+      author='The PEtab developers',
+      author_email='daniel.weindl@helmholtz-muenchen.de',
+      url='https://github.com/icb-dcm/petab',
+      packages=find_packages(exclude=['doc*', 'test*']),
       scripts=['bin/petablint.py'],
       install_requires=['numpy>=1.15.1',
                         'pandas>=0.23.4',
                         'matplotlib>=2.2.3',
+                        'python-libsbml>=5.17.0',
                         'sympy'],
-      tests_require=['pytest']
+      tests_require=['pytest'],
+      python_requires='>=3.6'
       )
