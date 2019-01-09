3b9b2acf44e12871b04ad665e0ad8ac21cfdc754
Carolin Loos <carolin.loos@helmholtz-muenchen.de>
2019-01-09 19:02:29 +0100

    changed path

--- visualization/test_experimentId.py
+++ visualization/test_experimentId.py
@@ -1,11 +1,9 @@
 import pandas as pd
 import generate_experimentId
 
-#DataFilePath = "../../Benchmark-Models/hackathon_contributions_new_data_format/Fujita_SciSignal2010/measurementData_Fujita_SciSignal2010.tsv"
-#ConditionFilePath = "../../Benchmark-Models/hackathon_contributions_new_data_format/Fujita_SciSignal2010/experimentalCondition_Fujita_SciSignal2010.tsv"
 
-DataFilePath = "../../Benchmark-Models/hackathon_contributions_new_data_format/Sobotta_Frontiers2017/measurementData_Sobotta_Frontiers2017.tsv"
-ConditionFilePath = "../../Benchmark-Models/hackathon_contributions_new_data_format/Sobotta_Frontiers2017/experimentalCondition_Sobotta_Frontiers2017.tsv"
+DataFilePath = "Sobotta_Frontiers2017/measurementData_Sobotta_Frontiers2017.tsv"
+ConditionFilePath = "Sobotta_Frontiers2017/experimentalCondition_Sobotta_Frontiers2017.tsv"
 
 
 measurement_data = pd.DataFrame.from_csv(
