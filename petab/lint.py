f287f0fba8f43a3557ad391d6f7bb06e806d6591
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2019-03-20 14:27:14 +0100

    not check bounds if parameter is not estimated

--- petab/lint.py
+++ petab/lint.py
@@ -172,11 +172,12 @@ def check_parameterBounds(parameter_df):
     in the parameter table.
     """
     for element in range(len(parameter_df['lowerBound'])):
-        if not parameter_df['lowerBound'][element] \
-                <= parameter_df['upperBound'][element]:
-            raise AssertionError(
-                f"lowerbound larger than upperBound in parameterId "
-                f"{parameter_df.index[element]}.")
+        if int(parameter_df['estimate'][element]) is True:
+            if not parameter_df['lowerBound'][element] \
+                    <= parameter_df['upperBound'][element]:
+                raise AssertionError(
+                    f"lowerbound larger than upperBound in parameterId "
+                    f"{parameter_df.index[element]}.")
 
 
 def assert_parameter_estimate_is_boolean(parameter_df):
