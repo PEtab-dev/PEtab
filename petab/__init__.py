b5dec9de41e505ab6a8b7caa4f10b9612b88b103
Dantong Wang <dantong.wang@helmholtz-muenchen.de>
2019-03-25 12:11:10 +0100

    add 'noqa: F403, F401' in init.py to avoid 'imported but unused' error

--- petab/__init__.py
+++ petab/__init__.py
@@ -1,5 +1,5 @@
 from .core import *  # noqa: F403, F401
 from .lint import *  # noqa: F403, F401
 from .sbml import *  # noqa: F403, F401
-from .generate_experiment_id import generate_experiment_id
+from .generate_experiment_id import generate_experiment_id  # noqa: F403, F401
 from .version import __version__  # noqa: F401
