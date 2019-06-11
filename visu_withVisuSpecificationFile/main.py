7310d745f9221b7f26d17f8f0f4c799d6c832809
erikadudki <edudkin@uni-bonn.de>
2019-06-11 08:39:08 +0200

    now: function which should be used, to plot data

--- visu_withVisuSpecificationFile/main.py
+++ visu_withVisuSpecificationFile/main.py
@@ -7,151 +7,137 @@ import petab
 import seaborn as sns
 sns.set()
 
-"""
-Run this script for plotting data and simulations.
 
-What exactly should be plotted is specified in a
-visualizationSpecification.tsv file.
+def plot_data_and_simulation(data_file_path, condition_file_path,
+                             visualization_file_path, simulation_file_path):
+    """
+    main function for plotting data and simulations.
 
-Also, the data, simulations and conditions have to be defined in a specific
-format (see "doc/documentation_data_format.md").
-"""
+    What exactly should be plotted is specified in a
+    visualizationSpecification.tsv file.
 
-data_file_path = "https://raw.githubusercontent.com/LoosC/Benchmark-Models/" \
-    "hackathon/hackathon_contributions_new_data_format/" \
-    "Isensee_JCB2018/measurementData_Isensee_JCB2018.tsv"
+    Also, the data, simulations and conditions have
+    to be defined in a specific format
+    (see "doc/documentation_data_format.md").
+    """
 
-condition_file_path = "https://raw.githubusercontent.com/LoosC/" \
-    "Benchmark-Models/hackathon/hackathon_contributions_" \
-    "new_data_format/Isensee_JCB2018/" \
-    "experimentalCondition_Isensee_JCB2018.tsv"
+    # Set Options for plots
+    # possible options: see: plt.rcParams.keys()
+    plt.rcParams['font.size'] = 10
+    plt.rcParams['axes.titlesize'] = 10
+    plt.rcParams['figure.figsize'] = [20, 10]
+    plt.rcParams['errorbar.capsize'] = 2
+    plt.plot_simulation = True
 
-visualization_file_path = "https://raw.githubusercontent.com/LoosC/"\
-    "Benchmark-Models/visualization/hackathon_contributions"\
-    "_new_data_format/Isensee_JCB2018/visualizationSpecific"\
-    "ation_Isensee_JCB2018_2.tsv"
-simulation_file_path = "https://raw.githubusercontent.com/LoosC/"\
-    "Benchmark-Models/visualization/hackathon_contributions"\
-    "_new_data_format/Isensee_JCB2018/simulationData"\
-    "_Isensee_JCB2018.tsv"
+    subplots = True
 
-# Set Options for plots
-# possible options: see: plt.rcParams.keys()
-plt.rcParams['font.size'] = 10
-plt.rcParams['axes.titlesize'] = 10
-plt.rcParams['figure.figsize'] = [20, 10]
-plt.rcParams['errorbar.capsize'] = 2
-plt.plot_simulation = True
+    # import measurement data, experimental condition, visualization
+    # specification, simulation data
+    measurement_data = petab.get_measurement_df(data_file_path)
+    experimental_condition = petab.get_condition_df(condition_file_path)
+    visualization_specification = pd.read_csv(
+        visualization_file_path, sep="\t", index_col=None)
+    simulation_data = pd.read_csv(
+        simulation_file_path, sep="\t", index_col=None)
 
-subplots = True
+    # Set Colormap
+    # ccodes = \
+    # ['#8c510a','#bf812d','#dfc27d','#f6e8c3', \
+    #  '#c7eae5','#80cdc1','#35978f','#01665e']
+    sns.set_palette("colorblind")
 
-# import measurement data, experimental condition, visualization
-# specification, simulation data
-measurement_data = petab.get_measurement_df(data_file_path)
-experimental_condition = petab.get_condition_df(condition_file_path)
-visualization_specification = pd.read_csv(
-    visualization_file_path, sep="\t", index_col=None)
-simulation_data = pd.read_csv(
-    simulation_file_path, sep="\t", index_col=None)
+    # get unique plotIDs
+    uni_plot_ids, plot_ind = np.unique(
+        visualization_specification.plotId, return_index=True)
 
-# Set Colormap
-# ccodes = \
-# ['#8c510a','#bf812d','#dfc27d','#f6e8c3', \
-#  '#c7eae5','#80cdc1','#35978f','#01665e']
-sns.set_palette("colorblind")
-
-# get unique plotIDs
-uni_plotIds, plotInd = np.unique(
-    visualization_specification.plotId, return_index=True)
-
-# Initiate subplots
-if subplots:
-    num_subplot = len(uni_plotIds)
-else:
-    num_subplot = 1
-
-num_row = np.round(np.sqrt(num_subplot))
-num_col = np.ceil(num_subplot / num_row)
-
-fig, ax = plt.subplots(int(num_row), int(num_col), squeeze=False)
-
-# loop over unique plotIds
-for i_plot_id, var_plot_id in enumerate(uni_plotIds):
-
-    # setting axis indices
+    # Initiate subplots
     if subplots:
-        axx = int(np.ceil((i_plot_id + 1) / num_col)) - 1
-        axy = int(((i_plot_id + 1) - axx * num_col)) - 1
+        num_subplot = len(uni_plot_ids)
     else:
-        axx = 0
-        axy = 0
+        num_subplot = 1
 
-    # get indices for specific plotId
-    ind_plot = visualization_specification['plotId'] == var_plot_id
+    num_row = np.round(np.sqrt(num_subplot))
+    num_col = np.ceil(num_subplot / num_row)
 
-    for i in visualization_specification[ind_plot].index.values:
-        # get datasetID and independent variable of first entry of plot1
-        dataset_id = visualization_specification.datasetId[i]
-        indep_var = visualization_specification.xValues[i]
+    fig, ax = plt.subplots(int(num_row), int(num_col), squeeze=False)
 
-        # define index to reduce measurement_data to data linked to datasetId
-        ind_dataset = measurement_data['datasetId'] == dataset_id
+    # loop over unique plotIds
+    for i_plot_id, var_plot_id in enumerate(uni_plot_ids):
 
-        # gather simulationConditionIds belonging to datasetId
-        uni_condition_id = np.unique(
-            measurement_data[ind_dataset].simulationConditionId)
-        clmn_name_unique = 'simulationConditionId'
+        # setting axis indices
+        if subplots:
+            axx = int(np.ceil((i_plot_id + 1) / num_col)) - 1
+            axy = int(((i_plot_id + 1) - axx * num_col)) - 1
+        else:
+            axx = 0
+            axy = 0
 
-        # Case seperation indepParameter custom, time or condition
-        if indep_var not in ['time', "condition"]:
+        # get indices for specific plotId
+        ind_plot = visualization_specification['plotId'] == var_plot_id
 
-            # extract conditions (plot input) from condition file
-            ind_cond = experimental_condition.index.isin(uni_condition_id)
-            conditions = experimental_condition[ind_cond][indep_var]
+        for i_visu_spec in visualization_specification[ind_plot].index.values:
+            # get datasetID and independent variable of first entry of plot1
+            dataset_id = visualization_specification.datasetId[i_visu_spec]
+            indep_var = visualization_specification.xValues[i_visu_spec]
 
-            ms = get_data_to_plot.get_data_to_plot(
-                visualization_specification, measurement_data,
-                simulation_data, uni_condition_id, i, clmn_name_unique)
+            # define index to reduce measurement_data to data linked to datasetId
+            ind_dataset = measurement_data['datasetId'] == dataset_id
 
-            ax = plotting_config.plotting_config(
-                visualization_specification, ax, axx, axy, conditions,
-                ms, ind_plot, i, plt)
+            # gather simulationConditionIds belonging to datasetId
+            uni_condition_id = np.unique(
+                measurement_data[ind_dataset].simulationConditionId)
+            clmn_name_unique = 'simulationConditionId'
 
-        elif indep_var == 'condition':
+            # Case seperation indepParameter custom, time or condition
+            if indep_var not in ['time', "condition"]:
 
-            ms = get_data_to_plot.get_data_to_plot(
-                visualization_specification, measurement_data,
-                simulation_data, uni_condition_id, i, clmn_name_unique)
+                # extract conditions (plot input) from condition file
+                ind_cond = experimental_condition.index.isin(uni_condition_id)
+                conditions = experimental_condition[ind_cond][indep_var]
 
-            ax = plotting_config.plotting_config(
-                visualization_specification, ax, axx, axy, conditions, ms,
-                ind_plot, i, plt)
+                ms = get_data_to_plot.get_data_to_plot(
+                    visualization_specification, measurement_data,
+                    simulation_data, uni_condition_id, i_visu_spec, clmn_name_unique)
 
-        elif indep_var == 'time':
+                ax = plotting_config.plotting_config(
+                    visualization_specification, ax, axx, axy, conditions,
+                    ms, ind_plot, i_visu_spec, plt)
 
-            # obtain unique observation times
-            uni_times = np.unique(measurement_data[ind_dataset].time)
+            elif indep_var == 'condition':
 
-            clmn_name_unique = 'time'
+                ms = get_data_to_plot.get_data_to_plot(
+                    visualization_specification, measurement_data,
+                    simulation_data, uni_condition_id, i_visu_spec, clmn_name_unique)
 
-            # group measurement values for each conditionId/unique time
-            ms = get_data_to_plot.get_data_to_plot(
-                visualization_specification, measurement_data,
-                simulation_data, uni_times, i, clmn_name_unique)
+                ax = plotting_config.plotting_config(
+                    visualization_specification, ax, axx, axy, conditions, ms,
+                    ind_plot, i_visu_spec, plt)
 
-            ax = plotting_config.plotting_config(
-                visualization_specification, ax, axx, axy, uni_times, ms,
-                ind_plot, i, plt)
+            elif indep_var == 'time':
 
-    ax[axx, axy].set_xlabel(visualization_specification.xLabel[i])
-    ax[axx, axy].set_ylabel(visualization_specification.yLabel[i])
+                # obtain unique observation times
+                uni_times = np.unique(measurement_data[ind_dataset].time)
 
-    if subplots is False:
-        filename = 'Plot' + str(i_plot_id) + '.png'
-        plt.savefig(filename)
-        ax[0, 0].clear()
+                clmn_name_unique = 'time'
 
-if subplots:
-    plt.savefig("Plot")
+                # group measurement values for each conditionId/unique time
+                ms = get_data_to_plot.get_data_to_plot(
+                    visualization_specification, measurement_data,
+                    simulation_data, uni_times, i_visu_spec, clmn_name_unique)
 
-plt.show()
+                ax = plotting_config.plotting_config(
+                    visualization_specification, ax, axx, axy, uni_times, ms,
+                    ind_plot, i_visu_spec, plt)
+
+        ax[axx, axy].set_xlabel(visualization_specification.xLabel[i_visu_spec])
+        ax[axx, axy].set_ylabel(visualization_specification.yLabel[i_visu_spec])
+
+        if subplots is False:
+            filename = 'Plot' + str(i_plot_id) + '.png'
+            plt.savefig(filename)
+            ax[0, 0].clear()
+
+    if subplots:
+        plt.savefig("Plot")
+
+    return ax
