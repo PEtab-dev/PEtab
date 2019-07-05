e408a325c36b0c2f457c0d3bcc2a70a6bd6fb35c
erikadudki <edudkin@uni-bonn.de>
2019-07-05 09:17:09 +0200

    add test example for visualization with specification file routine

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -4,6 +4,29 @@ import os
 
 sys.path.append(os.getcwd())
 from petab import generate_experiment_id  # noqa: E402
+from visu_withSpecificationFile import main
+
+
+def test_visualization_routine():
+    data_file_path = "https://raw.githubusercontent.com/LoosC/Benchmark-Models/" \
+        "hackathon/hackathon_contributions_new_data_format/" \
+        "Isensee_JCB2018/measurementData_Isensee_JCB2018.tsv"
+
+    condition_file_path = "https://raw.githubusercontent.com/LoosC/" \
+        "Benchmark-Models/hackathon/hackathon_contributions_" \
+        "new_data_format/Isensee_JCB2018/" \
+        "experimentalCondition_Isensee_JCB2018.tsv"
+
+    visualization_file_path = "https://raw.githubusercontent.com/LoosC/"\
+        "Benchmark-Models/visualization/hackathon_contributions"\
+        "_new_data_format/Isensee_JCB2018/visualizationSpecific"\
+        "ation_Isensee_JCB2018_2.tsv"
+    simulation_file_path = "https://raw.githubusercontent.com/LoosC/"\
+        "Benchmark-Models/visualization/hackathon_contributions"\
+        "_new_data_format/Isensee_JCB2018/simulationData"\
+        "_Isensee_JCB2018.tsv"
+    ax = main.plot_data_and_simulation(data_file_path, condition_file_path,
+                                   visualization_file_path, simulation_file_path)
 
 
 def test_generate_experimentId_no_empty():
