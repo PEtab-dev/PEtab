506d1f90f2c6566645f82f0368c9fb72e90d0c3d
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-12-02 21:50:07 +0100

    master doc

--- doc/conf.py
+++ doc/conf.py
@@ -45,6 +45,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = 'index'
+
 autosummary_generate = True
 
 autodoc_default_options = {
@@ -66,3 +68,11 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_context = {
+    "display_github": True,
+    "github_user": "ICB-DCM",
+    "github_repo": "petab",
+    "github_version": "develop",
+    "conf_py_path": "/doc",
+}
