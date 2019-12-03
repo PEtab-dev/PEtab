450f845feb4b24bfe354f13b4f74e5a1de376878
Daniel Weindl <daniel.weindl@helmholtz-muenchen.de>
2019-12-03 11:03:01 +0100

    Remove obsolete generate_experiment_id.py (Closes #111)

--- petab/__init__.py
+++ petab/__init__.py
@@ -4,5 +4,4 @@ from .core import *  # noqa: F403, F401
 from .lint import *  # noqa: F403, F401
 from .sbml import *  # noqa: F403, F401
 from .parameter_mapping import *  # noqa: F403, F401
-from .generate_experiment_id import generate_experiment_id  # noqa: F403, F401
 from .version import __version__  # noqa: F401
