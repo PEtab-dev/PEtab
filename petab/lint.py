b83310244dc264365f6e857542af8af8ce218df7
elba.raimundez <elba.raimundez@helmholtz-muenchen.de>
2019-12-12 15:21:55 +0100

    added assert_parameter_values_and_bounds_are_positive

--- petab/lint.py
+++ petab/lint.py
@@ -106,6 +106,7 @@ def check_parameter_df(
     assert_parameter_id_is_string(df)
     assert_parameter_scale_is_valid(df)
     assert_parameter_values_and_bounds_are_numeric(df)
+    assert_parameter_values_and_bounds_are_positive(df)
     assert_parameter_estimate_is_boolean(df)
     assert_parameter_id_is_unique(df)
     check_parameter_bounds(df)
@@ -220,6 +221,17 @@ def assert_parameter_values_and_bounds_are_numeric(parameter_df):
     parameter_df["lowerBound"].apply(float).all()
     parameter_df["upperBound"].apply(float).all()
     parameter_df["nominalValue"].apply(float).all()
+
+
+def assert_parameter_values_and_bounds_are_positive(parameter_df):
+    """
+    Check if all entries in the nominalValue, lowerBound and upperBound columns of the
+    parameter table are positive values.
+    """
+
+    (parameter_df["lowerBound"] > 0).all()
+    (parameter_df["upperBound"] > 0).all()
+    (parameter_df["nominalValue"] > 0).all()
     
 
 def check_parameter_bounds(parameter_df):
