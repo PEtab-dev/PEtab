bdb1bb1781721c7a127284c55b975b00caa65cea
yannikschaelte <yannik.schaelte@gmail.com>
2020-01-19 22:32:07 +0100

    implement sampling

--- petab/sampling.py
+++ petab/sampling.py
@@ -100,7 +100,8 @@ def sample_parameter_startpoints(parameter_df: pd.DataFrame,
         np.random.seed(seed)
 
     # get types and parameters of priors from dataframe
-    prior_list = parameters.get_priors_from_df(parameter_df)
+    prior_list = parameters.get_priors_from_df(
+        parameter_df, mode='initialization')
 
     startpoints = [sample_from_prior(prior, n_starts) for prior in prior_list]
 
