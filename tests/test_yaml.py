423214b3ebce98af287c044a627ee39acccb41e1
yannikschaelte <yannik.schaelte@gmail.com>
2020-01-20 15:50:30 +0100

    add format_version

--- tests/test_yaml.py
+++ tests/test_yaml.py
@@ -8,9 +8,12 @@ from jsonschema.exceptions import ValidationError
 
 def test_validate():
     data = {
-        'petab_version': '1.1.1'
+        'format_version': '1'
     }
 
     # should fail because we miss some information
     with pytest.raises(ValidationError):
         validate(data)
+    
+    data = "doc/example/example_Fujita/Fujita.yaml"
+    validate(data)
