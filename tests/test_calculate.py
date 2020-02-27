ad819e8ed2a12a8ea61f0664219b53ac7670b97c
Yannik Schälte <31767307+yannikschaelte@users.noreply.github.com>
2020-02-27 12:31:29 +0100

    Allow parametric noise overrides in noise formula for residuals (#347)
    * gitignore tmp files
    
    * fix docs
    
    * add residual computation function
    
    * fix flake8
    
    * extend test coverage
    
    * address reviewer comments
    
    * fix codacy
    
    * make codacy happier
    
    * allow parametric noise formulas
    
    * address reviewer comments

--- tests/test_calculate.py
+++ tests/test_calculate.py
@@ -4,6 +4,7 @@ from petab import calculate_residuals
 from petab.C import *
 import pandas as pd
 import numpy as np
+import pytest
 
 
 def test_calculate_residuals():
@@ -21,22 +22,28 @@ def test_calculate_residuals():
         NOISE_FORMULA: [2, 3]
     }).set_index([OBSERVABLE_ID])
 
+    parameter_df = pd.DataFrame(data={
+        PARAMETER_ID: ['par1', 'par2'],
+        NOMINAL_VALUE: [3, 4]
+    })
+
     simulation_df = measurement_df.copy(deep=True).rename(
         columns={MEASUREMENT: SIMULATION})
     simulation_df[SIMULATION] = [2, 2, 19, 20]
 
     residual_dfs = calculate_residuals(
-        measurement_df, simulation_df, observable_df)
+        measurement_df, simulation_df, observable_df, parameter_df)
 
-    assert set(residual_dfs[0][RESIDUAL]) == \
-        {(2-0)/2, (2-1)/2, (19-20)/3, (20-22)/3}
+    assert set(residual_dfs[0][RESIDUAL]) == pytest.approx(
+        {(2-0)/2, (2-1)/2, (19-20)/3, (20-22)/3})
 
     # don't apply normalization
     residual_dfs = calculate_residuals(
-        measurement_df, simulation_df, observable_df, normalize=False)
+        measurement_df, simulation_df, observable_df,
+        parameter_df, normalize=False)
 
-    assert set(residual_dfs[0][RESIDUAL]) == \
-        {(2-0), (2-1), (19-20), (20-22)}
+    assert set(residual_dfs[0][RESIDUAL]) == pytest.approx(
+        {(2-0), (2-1), (19-20), (20-22)})
 
 
 def test_calculate_residuals_replicates():
@@ -54,14 +61,19 @@ def test_calculate_residuals_replicates():
         NOISE_FORMULA: [2]
     }).set_index([OBSERVABLE_ID])
 
+    parameter_df = pd.DataFrame(data={
+        PARAMETER_ID: ['par1', 'par2'],
+        NOMINAL_VALUE: [3, 4]
+    }).set_index([PARAMETER_ID])
+
     simulation_df = measurement_df.copy(deep=True).rename(
         columns={MEASUREMENT: SIMULATION})
     simulation_df[SIMULATION] = [2, 2]
 
     residual_dfs = calculate_residuals(
-        measurement_df, simulation_df, observable_df)
+        measurement_df, simulation_df, observable_df, parameter_df)
 
-    assert set(residual_dfs[0][RESIDUAL]) == {(2-0)/2, (2-1)/2}
+    assert set(residual_dfs[0][RESIDUAL]) == pytest.approx({(2-0)/2, (2-1)/2})
 
 
 def test_calculate_residuals_scaling():
@@ -80,12 +92,52 @@ def test_calculate_residuals_scaling():
         NOISE_FORMULA: [2]
     }).set_index([OBSERVABLE_ID])
 
+    parameter_df = pd.DataFrame(data={
+        PARAMETER_ID: ['par1', 'par2'],
+        NOMINAL_VALUE: [3, 4]
+    }).set_index([PARAMETER_ID])
+
     simulation_df = measurement_df.copy(deep=True).rename(
         columns={MEASUREMENT: SIMULATION})
     simulation_df[SIMULATION] = [2, 3]
 
     residual_dfs = calculate_residuals(
-        measurement_df, simulation_df, observable_df)
+        measurement_df, simulation_df, observable_df, parameter_df)
 
-    assert set(residual_dfs[0][RESIDUAL]) == \
-        {(np.log(2)-np.log(0.5))/2, (np.log(3)-np.log(1))/2}
+    assert set(residual_dfs[0][RESIDUAL]) == pytest.approx(
+        {(np.log(2)-np.log(0.5))/2, (np.log(3)-np.log(1))/2})
+
+
+def test_calculate_residuals_non_numeric_overrides():
+    """Test calculate.calculate_residuals with non-numeric noise formula
+    overrides."""
+    measurement_df = pd.DataFrame(data={
+        OBSERVABLE_ID: ['obs_a', 'obs_a'],
+        SIMULATION_CONDITION_ID: ['c0', 'c0'],
+        TIME: [5, 10],
+        MEASUREMENT: [0.5, 1],
+        NOISE_PARAMETERS: ['7;8', '2;par1']
+    })
+
+    observable_df = pd.DataFrame(data={
+        OBSERVABLE_ID: ['obs_a'],
+        OBSERVABLE_FORMULA: ['A'],
+        OBSERVABLE_TRANSFORMATION: [LOG],
+        NOISE_FORMULA: ['2*noiseParameter1_obs_a + '
+                        'noiseParameter2_obs_a + par2']
+    }).set_index([OBSERVABLE_ID])
+
+    parameter_df = pd.DataFrame(data={
+        PARAMETER_ID: ['par1', 'par2'],
+        NOMINAL_VALUE: [3, 4]
+    }).set_index([PARAMETER_ID])
+
+    simulation_df = measurement_df.copy(deep=True).rename(
+        columns={MEASUREMENT: SIMULATION})
+    simulation_df[SIMULATION] = [2, 3]
+
+    residual_dfs = calculate_residuals(
+        measurement_df, simulation_df, observable_df, parameter_df)
+
+    assert set(residual_dfs[0][RESIDUAL]) == pytest.approx(
+        {(np.log(2)-np.log(0.5))/(2*7+8+4), (np.log(3)-np.log(1))/(2*2+3+4)})
