7c9dd2e55193bb74263d35014393a4e581f5b7a2
yannikschaelte <yannik.schaelte@gmail.com>
2020-11-17 22:41:59 +0100

    fix docs

--- doc/compile_tutorial.sh
+++ doc/compile_tutorial.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Build a pdflatex document from the documentation
+# Build a pdflatex document from the tutorial
 
-# format A3
 pandoc tutorial.rst \
     -o tutorial.pdf \
     -V geometry:margin=0.5in -V geometry:a4paper -V fontsize=10pt \
